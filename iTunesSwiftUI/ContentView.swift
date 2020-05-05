613d23f38496e7dcc708aefac69c91c30beef588
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-05-05 09:32:10 -0700

    Added the text alignment modifier via the preview, but it was incorrectly placed before bold(), which only exists on Text views!

--- iTunesSwiftUI/ContentView.swift
+++ iTunesSwiftUI/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
         VStack() {
             Text("Artist Name Is Really  Long Placeholder")
                 .font(.largeTitle)
+                .multilineTextAlignment(.center)
                 .bold()
                 .lineLimit(3)
         }
